d828db0cd9b7a0ba9e44a8cc121c282006f2a9f3
Ivan Dejanovic <idejanovic@gmail.com>
2019-03-10 19:25:05 +0100

    Added try pug template.

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,10 @@ function docs(req, res, locale) {
   res.render('docs', locale);
 }
 
+function tryMlpl(req, res, locale) {
+  res.render('try', locale);
+}
+
 function indexEn(req, res) {
   index(req, res, locales.en);
 }
@@ -33,6 +37,18 @@ function docsEs(req, res) {
   docs(req, res, locales.es);
 }
 
+function tryEn(req, res) {
+  tryMlpl(req, res, locales.en);
+}
+
+function tryRs(req, res) {
+  tryMlpl(req, res, locales.rs);
+}
+
+function tryEs(req, res) {
+  tryMlpl(req, res, locales.es);
+}
+
 function setRoutes(app) {
   app.get('/', indexEn);
   app.get('/index', indexEn);
@@ -41,6 +57,9 @@ function setRoutes(app) {
   app.get('/docs', docsEn);
   app.get('/docs_rs', docsRs);
   app.get('/docs_es', docsEs);
+  app.get('/try', tryEn);
+  app.get('/try_rs', tryRs);
+  app.get('/try_es', tryEs);
 
   errors.setErrors(app);
 }
